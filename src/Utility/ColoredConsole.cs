621e29cd5e1b61f92a71866bf060c58e6e07b85e
John <vim2meta@tutanota.com>
2018-02-06 16:36:40 -0500

    needs to be async to avoid console data race

--- src/Utility/ColoredConsole.cs
+++ src/Utility/ColoredConsole.cs
@@ -5,17 +5,15 @@ namespace FFA.Utility
 {
     internal static class ColoredConsole
     {
-        internal static Task WriteAsync(string message, ConsoleColor color)
+        internal static async Task WriteAsync(string message, ConsoleColor color)
         {
             var previous = Console.BackgroundColor;
 
             Console.BackgroundColor = color;
 
-            var task = Console.Out.WriteAsync(message);
+            await Console.Out.WriteAsync(message);
 
             Console.BackgroundColor = previous;
-
-            return task;
         }
 
         internal static Task WriteLineAsync(string message, ConsoleColor color)
