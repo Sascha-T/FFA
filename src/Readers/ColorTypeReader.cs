b07009e211a48c017c71cabfd957ba7075862d36
John <vim2meta@tutanota.com>
2018-02-07 19:31:06 -0500

    fix in parsing colors

--- src/Readers/ColorTypeReader.cs
+++ src/Readers/ColorTypeReader.cs
@@ -13,7 +13,7 @@ namespace FFA.Readers
         {
             var color = System.Drawing.Color.FromName(input);
 
-            if (color.ToArgb() == 0)
+            if (color.ToArgb() != 0)
             {
                 return Task.FromResult(TypeReaderResult.FromSuccess(new Discord.Color(color.R, color.G, color.B)));
             }
