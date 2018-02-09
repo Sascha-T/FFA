88cff57292776d1ad02153d9b7481b37fe091edb
vim2meta <vim2meta@tutanota.com>
2018-02-09 17:40:52 -0500

    proper hex formatting

--- src/Extensions/ColorExtensions.cs
+++ src/Extensions/ColorExtensions.cs
@@ -6,6 +6,6 @@ namespace FFA.Extensions
     public static class ColorExtensions
     {
         public static string GetFormattedString(this Color color)
-            => $"#{color.RawValue.ToString("16").PadLeft(Configuration.MAX_HEX_LENGTH, '0')}";
+            => $"#{color.RawValue.ToString($"X{Configuration.MAX_HEX_LENGTH}")}";
     }
 }
