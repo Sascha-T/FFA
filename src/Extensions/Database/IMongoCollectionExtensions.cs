474497dc0279bd8005c213ff541cf34082cb72c9
vim2meta <vim2meta@tutanota.com>
2018-02-27 23:04:27 -0500

    more todo

--- src/Extensions/Database/IMongoCollectionExtensions.cs
+++ src/Extensions/Database/IMongoCollectionExtensions.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
+// TODO: more extensions for other models, such as AnyCustomCommandAsync
 namespace FFA.Extensions.Database
 {
     public static class IMongoCollectionExtensions
