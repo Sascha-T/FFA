f125406a6345d99779213f9dc790eeee3286e452
John <vim2meta@tutanota.com>
2018-03-05 12:54:36 -0500

    check activity in any mute async

--- src/Extensions/Database/MuteCollectionExtensions.cs
+++ src/Extensions/Database/MuteCollectionExtensions.cs
@@ -7,6 +7,6 @@ namespace FFA.Extensions.Database
     public static class MuteCollectionExtensions
     {
         public static Task<bool> AnyMuteAsync(this IMongoCollection<Mute> collection, ulong userId, ulong guildId)
-            => collection.AnyAsync(x => x.UserId == userId && x.GuildId == guildId);
+            => collection.AnyAsync(x => x.UserId == userId && x.GuildId == guildId && x.Active);
     }
 }
